e6546b9f955a523316b2d7d072f25cdf6059b99c
sLill <ssl@ksu.edu>
2016-09-08 23:52:17 -0500

    FinalCommit
    Basic concept is there. Tail doesn't follow like a snake yet and
    instructions have yet to be written.

--- snake.js
+++ snake.js
@@ -6,6 +6,33 @@ backBuffer.width = frontBuffer.width;
 backBuffer.height = frontBuffer.height;
 var backCtx = backBuffer.getContext('2d');
 var oldTime = performance.now();
+var gameOver = 0;
+var score = 0;
+
+var blackSquare = new Image();
+var apple = new Image();
+blackSquare.src = 'BlackSquare.png';
+apple.src = 'Apple.png';
+
+var appleTimer = 0;
+var appleLocationX = [];
+var appleLocationY = [];
+var appleCount = 0;
+
+var snakeFragmentsX = [];
+var snakeFragmentsY = [];
+snakeFragmentsX[0] = 0;
+snakeFragmentsY[0] = 0;
+
+var x = 0;
+var y = 0;
+
+var input = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+}
 
 /**
  * @function loop
@@ -22,8 +49,11 @@ function loop(newTime) {
   // Flip the back buffer
   frontCtx.drawImage(backBuffer, 0, 0);
 
-  // Run the next loop
-  window.requestAnimationFrame(loop);
+    // Run the next loop
+  if (gameOver == 0)
+  {
+      window.requestAnimationFrame(loop);
+  }
 }
 
 /**
@@ -36,14 +66,64 @@ function loop(newTime) {
  */
 function update(elapsedTime) {
 
-  // TODO: Spawn an apple periodically
-  // TODO: Grow the snake periodically
-  // TODO: Move the snake
-  // TODO: Determine if the snake has moved out-of-bounds (offscreen)
-  // TODO: Determine if the snake has eaten an apple
+    // TODO: Spawn an apple periodically
+    appleTimer = appleTimer + elapsedTime;
+    if (appleTimer > 2000)
+    {
+        appleLocationX[appleCount] = Math.floor(Math.random() * 480);
+        appleLocationY[appleCount] = Math.floor(Math.random() * 760);
+        appleTimer = 0;
+        appleCount = appleCount + 1;
+    }
+  // TODO: Grow the snake periodically (GROWS WHEN APPLE EATEN)
+    // TODO: Move the snake
+    if (input.up) {
+        for (i = 0; i < snakeFragmentsX.length; i++)
+        {
+            snakeFragmentsY[i] -= 1;
+        }
+    }
+    if (input.down) {
+        for (i = 0; i < snakeFragmentsX.length; i++)
+        {
+            snakeFragmentsY[i] += 1;
+        }
+    }
+    if (input.right) {
+        for (i = 0; i < snakeFragmentsX.length; i++)
+        {
+            snakeFragmentsX[i] += 1;
+        }
+    }
+    if (input.left) {
+        for (i = 0; i < snakeFragmentsX.length; i++)
+        {
+            snakeFragmentsX[i] -= 1;
+        }
+    }
+    // TODO: Determine if the snake has moved out-of-bounds (offscreen)
+    if (snakeFragmentsX[0] < 0 || snakeFragmentsX[0] > 480 || snakeFragmentsY[0] < 0 || snakeFragmentsY[0] > 760) 
+    {
+        gameOver = 1;
+     }
+    // TODO: Determine if the snake has eaten an apple
+    for (i = 0; i < appleCount; i++)
+    {
+        var xDifference = snakeFragmentsX[0] - appleLocationX[i];
+        var yDifference = snakeFragmentsY[0] - appleLocationY[i];
+        if((xDifference > -10 && xDifference < 10) && (yDifference > -10 && yDifference < 10))
+        {
+            score = score + 10;
+            appleLocationX[i] = -1;
+            
+            var snakeLength = snakeFragmentsX.length;
+            snakeFragmentsX[snakeLength] = snakeFragmentsX[snakeLength - 1];
+            snakeFragmentsY[snakeLength] = snakeFragmentsY[snakeLength - 1] + 15;
+        }
+    }
   // TODO: Determine if the snake has eaten its tail
   // TODO: [Extra Credit] Determine if the snake has run into an obstacle
-
+    frontCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
 }
 
 /**
@@ -55,8 +135,91 @@ function update(elapsedTime) {
 function render(elapsedTime) {
   backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
 
-  // TODO: Draw the game objects into the backBuffer
+    // TODO: Draw the game objects into the backBuffer
+  for (i = 0; i < snakeFragmentsX.length; i++)
+  {
+      backCtx.drawImage(blackSquare, snakeFragmentsX[i], snakeFragmentsY[i]);
 
+  }
+  for (j = 0; j < appleCount; j++)
+  {
+      if (appleLocationX[j] > -1)
+      {
+          backCtx.drawImage(apple, appleLocationX[j], appleLocationY[j]);
+      }
+  }
+
+  if(gameOver == 1)
+  {
+      backCtx.fillStyle = "red";
+      backCtx.font = "bold 25px Arial";
+      backCtx.fillText("Game Over", 300, 240);
+  }
+
+  backCtx.fillStyle = "blue";
+  backCtx.font = "16px Arial";
+  backCtx.fillText(score, 20, 20);
+
+}
+
+window.onkeydown = function (event) {
+    switch (event.keyCode) {
+        //up
+        case 38:
+        case 87:
+            input.up = true;
+            y -= 1;
+            break;
+
+            //left
+        case 37:
+        case 65:
+            input.left = true;
+            x -= 1;
+            break;
+
+            //right
+        case 39:
+        case 68:
+            input.right = true;
+            x += 1;
+            break;
+
+            //down
+        case 40:
+        case 83:
+            input.down = true;
+            y += 1;
+            break;
+    }
+}
+
+window.onkeyup = function (event) {
+    switch (event.keyCode) {
+        //up
+        case 38:
+        case 87:
+            input.up = false;
+            break;
+
+            //left
+        case 37:
+        case 65:
+            input.left = false;
+            break;
+
+            //right
+        case 39:
+        case 68:
+            input.right = false;
+            break;
+
+            //down
+        case 40:
+        case 83:
+            input.down = false;
+            break;
+    }
 }
 
 /* Launch the game */
